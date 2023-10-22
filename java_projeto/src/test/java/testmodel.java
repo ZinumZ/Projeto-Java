7e96b839a14227d9cd26b49d44e8d832de6d4950
Anderson Muniz <anderson.muniz8@gmail.com>
2023-10-22 19:10:08 -0400

    Testing_Classes

--- java_projeto/src/test/java/testmodel.java
+++ java_projeto/src/test/java/testmodel.java
@@ -1,11 +1,21 @@
 
 import com.example.model.Bolo;
+import com.example.model.Salgado;
+import com.example.model.Suco;
 
 public class testmodel {
     public static void main(String[] args) {
         Bolo b1 = new Bolo("Chocolate", "1", 1100, 2);
         
-        double preco = b1.calcularPreco();
-        System.out.println("Preço do bolo: " + preco);
+        double precoBolo = b1.calcularPreco();
+        System.out.println("Preço do bolo: " + precoBolo);
+
+        Suco s1 = new Suco("Laranja", "2", 250); //Tamanhos: 250,500,700
+        double precoSuco = s1.calcularPreco();
+        System.out.println("Preço do suco: " + precoSuco);
+
+        Salgado sal1 = new Salgado("Carne", "3", true);
+        double precoSalgado = sal1.calcularPreco();
+        System.out.println("Preço do salgado: " + precoSalgado);
     }
 }
